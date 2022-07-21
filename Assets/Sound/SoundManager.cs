dcd0361f8c15a4446b7f6fe741c0ff6d5d3ff614
totroroy <troy.davis@student.forsbergsskola.se>
2022-07-21 13:56:05 +0200

    Update SoundManager.cs

--- Assets/Sound/SoundManager.cs
+++ Assets/Sound/SoundManager.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMOD.Studio;
+/*
 
-public class SoundManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+    public class SoundManager : MonoBehaviour {
+        [SerializeField] private FMODUnity.EventReference[] music;
+        [SerializeField] private FMODUnity.EventReference ambience;
+        private EventInstance musicInstance, ambienceInstance;
+        
+        public void PlayAmbientMusic(){
+            ambienceInstance = FMODUnity.RuntimeManager.CreateInstance(ambienceInstance);
+            ambienceInstance.start();
+        }
+    
+        public void PlayMusic(int levelNumber){
+            musicInstance = FMODUnity.RuntimeManager.CreateInstance(musicInstance);
+            musicInstance.start();
+        }
+    
+        public void StopMusic(){
+            musicInstance.stop(STOP_MODE.ALLOWFADEOUT);
+        }
+    
+        public void Footsteps(){
+            FMODUnity.RuntimeManager.PlayOneShot("");
+        }
+
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+*/
